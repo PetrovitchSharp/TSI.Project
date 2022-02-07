0f7ffae85ac0d93a6d3b205a6d9f44b19f406c2c
PetrovitchSharp <korchagin_official@yahoo.com>
2022-02-07 16:27:27 +0400

    Preparation for filtration

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,12 +23,38 @@ namespace TSI.Controllers
 	        else
 	        {
 		        ViewBag.Cars = cars;
-		        ViewBag.Total = cars.Sum(cartItem => cartItem.Price);
-				
 	        }
 			return View();
         }
 
+	    public IActionResult Search(SearchQuery searchQuery)
+	    {
+		    var context = new DataContext();
+		    var cars = context.Cars
+			    .Where(car => car.Price <= searchQuery.MaxPrice && car.Price >= searchQuery.MinPrice)
+			    .Where(car => car.BodyType == searchQuery.BodyType)
+			    .Where(car => car.Consumption <= searchQuery.MaxConsumption && car.Consumption >= searchQuery.MinConsumption)
+			    .Where(car => car.Drive == searchQuery.Drive)
+			    .Where(car => car.Engine == searchQuery.Engine)
+			    .Where(car => car.Mileage <= searchQuery.MaxMileage && car.Mileage >= searchQuery.MinMileage)
+			    .Where(car => car.State == searchQuery.State)
+			    .Where(car => car.Transmission == searchQuery.Transmission)
+			    .Where(car => car.Year <= searchQuery.MaxYear && car.Year >= searchQuery.MinYear)
+			    .Where(car => car.Power <= searchQuery.MaxPower && car.Power >= searchQuery.MinPower)
+			    .OrderByDescending(car => car.Price)
+			    .ToList();
+
+		    if (!cars.Any())
+		    {
+			    ViewBag.Cars = new List<Car>();
+		    }
+		    else
+		    {
+			    ViewBag.Cars = cars;
+
+		    }
+		    return View("Index");
+}
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
---
+++ Models/SearchQuery.cs
@@ -0,0 +1,27 @@
+﻿#nullable enable
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
+
+namespace TSI.Models
+{
+	public class SearchQuery
+	{
+		public string State { get; set; }
+		public int MinYear { get; set; }
+		public int MaxYear { get; set; }
+		public int MinMileage { get; set; }
+		public int MaxMileage { get; set; }
+		public string BodyType { get; set; }
+		public string Transmission { get; set; }
+		public string Engine { get; set; }
+		public int MinPower { get; set; }
+		public int MaxPower { get; set; }
+		public string Drive { get; set; }
+		public double MinConsumption { get; set; }
+		public double MaxConsumption { get; set; }
+		public int MinPrice { get; set; }
+		public int MaxPrice { get; set; }
+	}
+}
